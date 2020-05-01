b076b77a886139a4075572f16706ff8ccfcb3414
Sakar Ghimire <sakar@kathmandu>
2020-05-01 19:17:08 +0545

    completed writing function to check for valid words

--- app/javascript/reactapp/App.js
+++ app/javascript/reactapp/App.js
@@ -31,27 +31,91 @@ const InputWord = (props) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    let wordIsValid = true;
-    let board =  Array(16).fill(0).map((e,i) => e + i + 1 );
+    // let wordIsValid = true;
+    let board =  Array(16).fill(0).map((e,i) => e + i);
     let validPositions = [...board];
-    console.log("entered word", [...word]); 
-    console.log("valid posi", [...validPositions]);
-    console.log("letters on board", [...props.lettersOnBoard]);
-    [...word.toUpperCase()].forEach((letter) => {
-      let selectedLetterPositions = props.lettersOnBoard.reduce((acc,l,i)=>(l === letter ? [...acc, i + 1] : acc ),[]);
-      selectedLetterPositions = selectedLetterPositions.filter((e)=>(validPositions.indexOf(e) !== -1));
-      console.log(letter + " is at position " , selectedLetterPositions);
-      if (selectedLetterPositions.length === 0) {
-        wordIsValid = false;
+    const validPosition = (e) => {
+      return
+    };
+
+    const vp = (e, b) => {
+
+    }
+
+    const updateValidPos = (slp,board) => {
+      let newPos = [];
+      slp.forEach((e) => {
+        let col1 = e % 4; 
+        let row1 = Math.floor(e / 4) ;
+        board.forEach((f) => {
+          let col2 = f % 4; 
+          let row2 = Math.floor(f / 4) ;
+          if(!(col1 === col2 && row1 === row2) && !(Math.abs(col1-col2) > 1 || Math.abs(row1-row2) > 1) ){
+            newPos = [...newPos,f]
+          }
+        });
+      });
+      return [...new Set(newPos)];
+    }
+    
+    const wordIsValid = (word,board,validPos) => {
+      
+      // return if reached end of word
+      if (word.length === 0) {
+        return true;
       }
-      if (wordIsValid) {
-       let validPositionsTemp = [];
 
+      let currentLetter = word[0];
+      // get all cell position of the current letter
+      let possiblePos = props.lettersOnBoard.reduce((acc,l,i)=>(l === currentLetter ? [...acc, i] : acc ),[]);
+      // filter all positions that are not adjacent to previous letter
+      possiblePos = possiblePos.filter((e)=>(validPos.indexOf(e) !== -1));
+      // if no cell position is valid then return false
+      if (possiblePos.length === 0) {
+        return false;
       }
 
-    });
+      // check all the combination of valid position that can produce the word
+      for(let index=0; index<possiblePos.length; index++) {
+        
+        // remove the selected position from the board
+        let newBoard = board.filter((e)=>(e !== possiblePos[index]));
+        // get all adjacent cells of the current cell
+        let newPossiblePos = updateValidPos([possiblePos[index]],newBoard);
+       
+        let newWord = word.slice(1);
+        
+        // checks if the next letter can be selected or not
+        if (wordIsValid(newWord,newBoard,newPossiblePos)) {
+          return true;
+        }
+      }
 
-    console.log("Word is valid", wordIsValid);
+      return false;
+     
+    }
+
+    let enteredWord = [...word.toUpperCase()];
+    
+
+    
+    // [...word.toUpperCase()].forEach((letter) => {
+    //   console.log("letters on board", [...props.lettersOnBoard]);
+    //   let selectedLetterPositions = props.lettersOnBoard.reduce((acc,l,i)=>(l === letter ? [...acc, i] : acc ),[]);
+    //   selectedLetterPositions = selectedLetterPositions.filter((e)=>(validPositions.indexOf(e) !== -1));
+    //   console.log(letter + " is at position " , selectedLetterPositions);
+    //   if (selectedLetterPositions.length === 0) {
+    //     wordIsValid = false;
+    //   }
+    //   if (wordIsValid) {
+    //    console.log("new valid", updateValidPos(selectedLetterPositions,board));
+    //    validPositions = updateValidPos(selectedLetterPositions,board);
+      
+    //   }
+
+    // });
+
+    // console.log("Word is valid", wordIsValid);
     props.addWord(word);
     setWord("");
   };
